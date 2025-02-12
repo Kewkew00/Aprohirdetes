444dac19197c4d1d48a03125d710be3486705b81
mojzeservin <113088620+mojzeservin@users.noreply.github.com>
2025-02-12 12:29:40 +0100

    tab change

--- Aprohirdetes/src/app/components/login/login.component.ts
+++ Aprohirdetes/src/app/components/login/login.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import {MatTabsModule} from '@angular/material/tabs';
+import { AfterViewInit, Component, ElementRef, ViewChild, viewChild } from '@angular/core';
+import {MatTabGroup, MatTabsModule} from '@angular/material/tabs';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
@@ -19,7 +19,7 @@ import { AuthService } from '../../services/auth.service';
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
-export class LoginComponent {
+export class LoginComponent implements AfterViewInit{
   hide = true;
   constructor
   (
@@ -43,6 +43,16 @@ export class LoginComponent {
     confirm: ''
   }
 
+  @ViewChild('.tabGroup') tabGroup!: MatTabGroup;
+
+  ngAfterViewInit(): void {
+    console.log(this.tabGroup)
+  }
+
+  navigateToLoginTab(){
+    this.tabGroup.selectedIndex = 0
+  }
+
   login() {
     this.api.login('users', this.user).subscribe((res:any) => {
 
@@ -59,19 +69,24 @@ export class LoginComponent {
   }
 
   register() {
-    this.api.registration('users', this.userReg).subscribe((res:any) => {
+    if (this.userReg.password!=this.userReg.confirm) {
+      console.log('A jelszavak nem egyeznek')
+      return;
+    }
+    else{
+      this.api.registration('users', this.userReg).subscribe((res:any) => {
 
-      if(res.success == true) {
-
-        this.message.showMessage('OK', res.message, 'success');
-      }else{
-
-        this.message.showMessage('HIBA', res.message, 'danger');
-      }
-      this.auth.saveTokenAndLogin(res.token)
-      this.router.navigate(['/'])
-    })
+        if(res.success == true) {
+  
+          this.message.showMessage('OK', res.message, 'success');
+        }else{
+  
+          this.message.showMessage('HIBA', res.message, 'danger');
+        }
+        this.auth.saveTokenAndLogin(res.token);
+        this.router.navigate(['/']);
+        this.navigateToLoginTab();
+      })
+    }
   }
-
-
 }
