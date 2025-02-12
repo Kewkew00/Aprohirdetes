df4a59cc18d003ccd9ccc445688200a4c253d57a
Kewkew00 <78853822+Kewkew00@users.noreply.github.com>
2025-02-12 10:59:29 +0100

    backend osszekotes frontenddel

--- Aprohirdetes/src/app/components/login/login.component.ts
+++ Aprohirdetes/src/app/components/login/login.component.ts
@@ -5,20 +5,50 @@ import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import { Router, RouterModule } from '@angular/router';
-import { HirdetesComponent } from '../hirdetes/hirdetes.component';
+import { User } from '../../interfaces/user';
+import { ApiService } from '../../services/api.service';
+import { MessageService } from '../../services/message.service';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatTabsModule, MatInputModule, MatFormFieldModule, MatIconModule, MatButtonModule, RouterModule],
+  imports: [MatTabsModule, MatInputModule, MatFormFieldModule, MatIconModule, MatButtonModule, CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
   hide = true;
-  constructor(private router: Router) {}
+  constructor
+  (
+    private api: ApiService,
+    private router: Router,
+    private auth: AuthService,
+    private message: MessageService
+  ) {}
 
-  navigateToHirdetes(): void {
-    this.router.navigate(['/hirdetes']);
+
+  user:User = {
+    email: '',
+    password: ''
   }
+
+  login() {
+    this.api.login('users', this.user).subscribe((res:any) => {
+
+      if(res.success == true) {
+
+        this.message.showMessage('OK', res.message, 'success');
+      }else{
+
+        this.message.showMessage('HIBA', res.message, 'danger');
+      }
+      this.auth.saveTokenAndLogin(res.token)
+      this.router.navigate(['/hirdetes'])
+    })
+  }
+
+
 }
