408fcf27495f6b7b38891e62d2974afe71b3f323
Kewkew00 <78853822+Kewkew00@users.noreply.github.com>
2025-02-17 09:08:10 +0100

    navbar comp & navigation

--- Aprohirdetes/src/app/app.routes.ts
+++ Aprohirdetes/src/app/app.routes.ts
@@ -1,9 +1,23 @@
 import { RouterModule, Routes } from '@angular/router';
 import { HirdetesComponent } from './components/hirdetes/hirdetes.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 export const routes: Routes = [
     {
+        path: 'login', component:LoginComponent
+    },
+
+    {
+        path: 'register', component:RegisterComponent
+    },
+
+    {
         path: 'hirdetes', component: HirdetesComponent 
+    },
+
+    {
+        path: '', redirectTo:'login', pathMatch:'full'
     }
 ];
 
